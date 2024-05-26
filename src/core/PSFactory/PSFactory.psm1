2ff375cd26a93697891e7f2f8482c10520c55a38
Flavien MICHALECZEK <fmichaleczek@gmail.com>
2024-05-26 14:31:59 +0200

    update

--- src/core/PSFactory/PSFactory.psm1
+++ src/core/PSFactory/PSFactory.psm1
@@ -2,35 +2,52 @@
 [CmdletBinding()]
 param()
 
-$intPaths = "$PSScriptRoot/enums", "$PSScriptRoot/classes", "$PSScriptRoot/internal"
-$pubPaths = "$PSScriptRoot/public"
+$script:ModuleName = $MyInvocation.MyCommand.ScriptBlock.Module.Name
+$itemSplat = @{ File = $true ; Recurse = $true ; Force = $true ; ErrorAction = 'Ignore' }
 
-Write-Verbose "[*] Searching script files..."
-$intFiles = Get-ChildItem -Path $intPaths -Filter *.ps1 -ErrorAction SilentlyContinue
-$pubFiles = Get-ChildItem -Path $pubPaths -Filter *.ps1 -ErrorAction SilentlyContinue
-$files = @($intFiles) + @($pubFiles)
-Write-Verbose "[-] Found $($files.Count) script file(s)."
+Write-Verbose '[*] Searching script files...'
+$internal = Get-ChildItem @itemSplat -Path $PSScriptRoot/enums, $PSScriptRoot/classes, $PSScriptRoot/internal -Filter *.ps1
+$public   = Get-ChildItem @itemSplat -Path $PSScriptRoot/public -Filter *.ps1
+$tasks    = Get-ChildItem @itemSplat -Path $PSScriptRoot/tasks -Filter *.build.ps1
 
-Write-Verbose "[*] Importing script files..."
-foreach ($file in $files.FullName)
+Write-Verbose '[*] Importing script files...'
+foreach ($file in (@($internal) + @($public)).FullName)
 {
     try
     {
-        Write-Verbose "`t - [*] Importing file '$file'"
+        Write-Verbose "[*] dot source file '$file'"
         . $file
     }
     catch
     {
-        Write-Error -Message "Failed to import file '$file', error: $_"
+        Write-Error -Message "Failed to dot source file '$file', Exception: $($_.Exception)"
     }
 }
 
-Write-Verbose "[*] Exporting Module Member..."
+Write-Verbose '[*] Importing tasks alias...'
+foreach ($file in @($tasks))
+{
+    Set-Alias -Name ('{0}.{1}.ib.tasks' -f $file.BaseName, $ModuleName) -Value $file.FullName
+}
+
+if (Test-Path $PSScriptRoot/scripts/Set-FactoryTaskVariable.ps1)
+{
+    Set-Alias -Name Set-FactoryTaskVariable -Value $PSScriptRoot/scripts/Set-FactoryTaskVariable.ps1
+    # Set-Alias -Name 'PSFactory.Tasks' -Value "$PSScriptRoot/build.tasks.ps1" -Force -ErrorAction Ignore
+}
+
+if (Test-Path $PSScriptRoot/Localization/$ModuleName.psd1)
+{
+    $script:LocalizedData = Import-LocalizedData -BaseDirectory $PSScriptRoot/Localization -FileName $ModuleName.psd1
+}
+
+
+Write-Verbose '[*] Exporting Module Member...'
 if ($Debug)
 {
     Export-ModuleMember -Function * -Alias * -Variable *
 }
 else
 {
-    Export-ModuleMember -Function $pubFiles.BaseName
+    Export-ModuleMember -Function $public.BaseName
 }
